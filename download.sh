4798b95b94c880e12db9c0da32ec0aaf952f00df
Michael Altobelli <maltob@users.noreply.github.com>
2024-11-25 14:25:09 -0500

    Merge in base version from OBS (#1)
    Added templates and update the CI to work with Github Actions via sudo and other prep

---
+++ download.sh
@@ -0,0 +1,2 @@
+mkdir input
+curl "https://cdn-fastly.obsproject.com/downloads/obs-studio-30.2.3-macos-intel.dmg" -o "input/obs-studio-30.2.3-macos-intel.dmg"
--- install_prereq_ubuntu.sh
+++ install_prereq_ubuntu.sh
@@ -1,10 +1,10 @@
 #assumes ubuntu:24.04
 
-apt update -y && apt install -y python3 curl xz-utils git build-essential autoconf libxml2-dev libssl-dev zlib1g-dev
-curl https://www.7-zip.org/a/7z2408-linux-x64.tar.xz -o 7z.tar.xz && tar -xf 7z.tar.xz 7zzs && cp 7zzs /usr/bin/7zzs && chmod +x /usr/bin/7zzs
+sudo apt update -y && sudo apt install -y python3 curl xz-utils git build-essential autoconf libxml2-dev libssl-dev zlib1g-dev
+curl https://www.7-zip.org/a/7z2408-linux-x64.tar.xz -o 7z.tar.xz && tar -xf 7z.tar.xz 7zzs && sudo cp 7zzs /usr/bin/7zzs && sudo chmod +x /usr/bin/7zzs
 git clone "https://github.com/maltob/bomutils.git" /tmp/bomutils
 git clone "https://github.com/mackyle/xar.git" /tmp/xar
 cd /tmp/bomutils/
-make && make install
+make && sudo make install
 cd /tmp/xar/xar
-sed -i 's/OpenSSL_add_all_ciphers/OPENSSL_init_crypto/g' configure.ac && ./autogen.sh && make && make install
+sed -i 's/OpenSSL_add_all_ciphers/OPENSSL_init_crypto/g' configure.ac && ./autogen.sh && make && sudo cp src/xar /usr/bin/xar && sudo chmod +x /usr/bin/xar
